012482481b0955c1ba4fb023f01f190ae8028165
Luan Nguyen Thanh <tklarryonline@gmail.com>
2015-08-09 04:01:37 +0700

    [r] Fixed test for see add income button

--- income/tests/views/ui/test_see_add_income_button.py
+++ income/tests/views/ui/test_see_add_income_button.py
@@ -1,11 +1,22 @@
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 from common.tests.live_server_test_case import BaseLiveTestCase
+from income.factories import IncomeRecordFactory
 
 
 class SeeAddIncomeTestCase(BaseLiveTestCase):
     def test_see_button(self):
         self.login_user()
+
+        incomes = []
+        for month in range(1, 13):
+            timestamp = timezone.datetime(year=2014, month=month, day=1)
+            timestamp = timezone.make_aware(timestamp)
+            income = IncomeRecordFactory(user=self.user, timestamp=timestamp)
+            incomes.append(income)
+
+        self.incomes = incomes
         self.visit(reverse('income:index'))
 
         self.link("Add income").should.be.ok
