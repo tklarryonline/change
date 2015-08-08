3b7ac3ddd578d86769b1b05ccbf9df2753b7bcee
Luan Nguyen Thanh <tklarryonline@gmail.com>
2015-08-08 14:59:03 +0700

    [f] 912 - Income record adding page should require login
    [https://www.pivotaltracker.com/story/show/100863912]

--- income/tests/views/ui/test_view_income_form.py
+++ income/tests/views/ui/test_view_income_form.py
@@ -1,16 +1,28 @@
 from django.core.urlresolvers import reverse
+from common.factories import UserFactory
 from common.tests.live_server_test_case import BaseLiveTestCase
+
+from income.models.records import IncomeRecord
 
 
 class TestViewIncomeForm(BaseLiveTestCase):
 
     def setUp(self):
+        self.user = UserFactory()
+        self.login(self.user)
         self.visit(page=reverse('income:add'))
 
     def test_view_income_form_page(self):
         self.should_see_text('Add your income record')
 
         self.find('#income-form').should.be.ok
-        self.find('#income-form input#id_number').should.be.ok
-        self.find('#income-form input#id_timestamp').should.be.ok
+        self.find('#income-form #id_number').should.be.ok
+        self.find('#income-form #id_timestamp').should.be.ok
         self.should_see_text('Add income record')
+
+    def test_add_income_page(self):
+        num_records = IncomeRecord.objects.count()
+        self.fill_in(selector='#id_number', value='10000')
+        self.fill_in(selector='#id_timestamp', value='2015-06-28')
+        self.button('Add income record').click()
+        IncomeRecord.objects.count().should.equal(num_records + 1)
--- income/urls.py
+++ income/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from income.views.add_income_view import AddIncomeView
 
@@ -6,7 +7,7 @@ from income.views.add_income_view import AddIncomeView
 urlpatterns = [
     url(
         regex=r'^add/',
-        view=AddIncomeView.as_view(),
+        view=login_required(AddIncomeView.as_view()),
         name='add'
     )
 ]
