0e891b4ad7e01f0482182aecaab38501e2b52288
Bang Dao <daotranbang@gmail.com>
2015-08-08 15:07:59 +0700

    [r] pep-8 format

--- income/urls.py
+++ income/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+
 from income.views.index_view import IndexView
+
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
--- income/views/index_view.py
+++ income/views/index_view.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.list import ListView
+
 from income.models.records import IncomeRecord
 
 
